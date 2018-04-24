231360c964861ea9924ebad1d95e1b70a0f75d0b
Kiran Telukunta Saraswathi <kiran.telukunta@indiayouth.info>
2018-04-24 02:04:39 +0200

    * RESTful - updated with progressbar and try except

--- RESTfulAPI/RESTful.client.get.py
+++ RESTfulAPI/RESTful.client.get.py
@@ -37,10 +37,49 @@
 #usage			:python3 RESTFul.client.get.py
 #==============================================================================
 
+from __future__ import print_function
 import urllib2
 import time
 import sys
 import getopt
+import sys
+import re
+
+
+class ProgressBar(object):
+    DEFAULT = 'Progress: %(bar)s %(percent)3d%%'
+    FULL = '%(bar)s %(current)d/%(total)d (%(percent)3d%%) %(remaining)d to go'
+
+    def __init__(self, total, width=40, fmt=DEFAULT, symbol='=', output=sys.stderr):
+        assert len(symbol) == 1
+
+        self.total = total
+        self.width = width
+        self.symbol = symbol
+        self.output = output
+        self.fmt = re.sub(r'(?P<name>%\(.+?\))d', r'\g<name>%dd' % len(str(total)), fmt)
+        self.current = 0
+
+    def __call__(self):
+        percent = self.current / float(self.total)
+        size = int(self.width * percent)
+        remaining = self.total - self.current
+        bar = '[' + self.symbol * size + ' ' * (self.width - size) + ']'
+        args = {
+            'total': self.total,
+            'bar': bar,
+            'current': self.current,
+            'percent': percent * 100,
+            'remaining': remaining
+        }
+        print('\r' + self.fmt % args, file=self.output, end='')
+
+    def done(self):
+        self.current = self.total
+        self()
+        print('', file=self.output)
+
+
 
 inputFile = ''
 bioconcept = ''
@@ -70,9 +109,24 @@ fh = open(inputFile)
 
 outPutFH = open(outPutFile, 'w')
 
+downloadProgress = ProgressBar(612508, fmt=ProgressBar.FULL)
+skipLines = 4356
+
 for pmid in fh:
 	#Submit
+	if skipLines > downloadProgress.current:
+		next(fh)
+		downloadProgress.current += 1
+		downloadProgress()
+
 	pmid=pmid.rstrip('\r\n')
 	url_Submit = "https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/" + bioconcept + "/" + pmid + "/" + format + "/"
-	urllib_result = urllib2.urlopen(url_Submit)
-	outPutFH.write(urllib_result.read())
+	try:
+		urllib_result = urllib2.urlopen(url_Submit)
+		outPutFH.write(urllib_result.read())
+		downloadProgress.current += 1
+		downloadProgress()
+	except urllib2.HTTPError:
+		time.sleep(120)
+		continue
+downloadProgress.done()
